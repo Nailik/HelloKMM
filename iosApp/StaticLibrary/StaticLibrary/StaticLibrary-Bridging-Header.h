dc88b374653f040d6c0203253d54d6d3fd38a430
Nailik <email@kilianeller.de>
2022-05-24 15:11:47 +0200

    added briding header to static library for cinteropt usage

---
+++ iosApp/StaticLibrary/StaticLibrary/StaticLibrary-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  StaticLibrary-Bridging-Header.h
+//  StaticLibrary
+//
+//  Created by Kilian Eller on 24.05.22.
+//
+
+#ifndef StaticLibrary_Bridging_Header_h
+#define StaticLibrary_Bridging_Header_h
+
+
+#endif /* StaticLibrary_Bridging_Header_h */
