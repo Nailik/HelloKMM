e2c7114544df895a98b6db1592d6a05deb77debb
Nailik <email@kilianeller.de>
2022-05-24 15:10:51 +0200

    added static library example implementation

---
+++ iosApp/StaticLibrary/StaticLibrary/StaticLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  StaticLibrary.swift
+//  StaticLibrary
+//
+//  Created by Kilian Eller on 24.05.22.
+//
+import Foundation
+
+@objc class StaticLibrary : NSObject {
+
+    @objc class func test() -> String {
+        return "testWorked!"
+    }
+
+}
