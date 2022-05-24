9b38afbade781644d94d52317aa0d75f3fd90fe5
Nailik <email@kilianeller.de>
2022-05-24 15:10:10 +0200

    removed unused files

--- shared/src/commonTest/kotlin/com/blakebarrett/hellokmm/UsingContextTest.kt
+++
@@ -1,4 +0,0 @@
-package com.blakebarrett.hellokmm
-
-@Suppress("NO_ACTUAL_FOR_EXPECT")
-expect abstract class UsingContextTest()
--- shared/src/iosTest/kotlin/com/blakebarrett/hellokmm/UsingContextTest.kt
+++
@@ -1,8 +0,0 @@
-package com.blakebarrett.hellokmm
-
-import kotlin.test.Test
-import kotlin.test.assertTrue
-
-actual abstract class UsingContextTest() {
-
-}
